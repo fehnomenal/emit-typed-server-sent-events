d6e3577b43a574c152b512314af530f8e80f781d
Andreas Fehn <fehnomenal@fehn.systems>
2024-04-19 13:27:11 +0200

    v2.0.1 @ c349f2ea77a194e056e0b0416c106374c39bfc2d

---
+++ dist/server.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="node" />
+import type { EventEmitter } from 'node:events';
+import type { DefaultCtx, EmitterEvents, EventsSwitchMap } from './types.ts';
+export declare const defineMapFor: <Emitter extends EventEmitter<[never]>>(emitter: Emitter) => MapDefiner<Emitter, DefaultCtx>;
+declare class MapDefiner<Emitter extends EventEmitter, Ctx> {
+    #private;
+    constructor(emitter: Emitter);
+    withContext<Context>(): MapDefiner<Emitter, Context>;
+    switchEvents<Map extends EventsSwitchMap<Ctx, EmitterEvents<Emitter>>>(map: Map): EventStreamer<Emitter, Ctx, Map>;
+}
+export declare class EventStreamer<Emitter extends EventEmitter, Ctx, Map extends EventsSwitchMap<Ctx, EmitterEvents<Emitter>>> {
+    #private;
+    constructor(emitter: Emitter, map: Map);
+    streamEvents(...[ctx]: [Ctx] extends [DefaultCtx] ? [] : [ctx: Ctx]): import("undici-types").Response;
+}
+export {};
